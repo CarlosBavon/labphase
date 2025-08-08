c580754ed1fd9ed509089fa66c5ffdf778b6dff4
Bavon <carlosbavon46@gmail.com>
2025-08-08 12:22:29 +0300

    Installed dependencies, mongoose, cors

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 // Initialize Express
 const app = express();
