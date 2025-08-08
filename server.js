9af7ceb97e9882b42a107c5d9309b25ec34b02f4
Bavon <carlosbavon46@gmail.com>
2025-08-08 13:42:22 +0300

    Configured Mongo to server and added neccessary API routes

--- server.js
+++ server.js
@@ -1,25 +1,51 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 // Initialize Express
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+app.use(cors());
+app.use(express.json());
 
 const hostname = process.env.HOST;
 const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.send('Welcome to labphase server!');
+//Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB 👅😝😜"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// Application Schema
+const postSchema = new mongoose.Schema({
+  caption: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to labphase server!");
+});
+
+// Get all applications (for admin view)
+app.get("/posts", async (req, res) => {
+  const posts = await Post.find();
+  res.json(posts);
+});
+app.post('/posts', async(req, res) => {
+  const { caption, imageUrl } = req.body;
+  const newPost = new Post ({ caption, imageUrl });
+  await newPost.save();
+  res.status(201).json(newPost);
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-    console.log('Press Ctrl+C to stop the server.');
-    console.log('Press Ctrl+R to restart the server.');
-    console.log('Waiting for requests...');
+  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log("Press Ctrl+C to stop the server.");
+  console.log("Press Ctrl+R to restart the server.");
+  console.log("Waiting for requests...");
 });
